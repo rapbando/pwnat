8cb9de9962f06cc2607b9023dee8ce861b60716c
rapbando <rapbando@hotmail.it>
2024-06-23 23:33:35 +0200

    forced 32bit compile CMakeLists.txt

---
+++ build/CMakeFiles/pwnat.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hacker/pwnat/client.c" "CMakeFiles/pwnat.dir/client.c.o" "gcc" "CMakeFiles/pwnat.dir/client.c.o.d"
+  "/home/hacker/pwnat/destination.c" "CMakeFiles/pwnat.dir/destination.c.o" "gcc" "CMakeFiles/pwnat.dir/destination.c.o.d"
+  "/home/hacker/pwnat/list.c" "CMakeFiles/pwnat.dir/list.c.o" "gcc" "CMakeFiles/pwnat.dir/list.c.o.d"
+  "/home/hacker/pwnat/message.c" "CMakeFiles/pwnat.dir/message.c.o" "gcc" "CMakeFiles/pwnat.dir/message.c.o.d"
+  "/home/hacker/pwnat/packet.c" "CMakeFiles/pwnat.dir/packet.c.o" "gcc" "CMakeFiles/pwnat.dir/packet.c.o.d"
+  "/home/hacker/pwnat/pwnat.c" "CMakeFiles/pwnat.dir/pwnat.c.o" "gcc" "CMakeFiles/pwnat.dir/pwnat.c.o.d"
+  "/home/hacker/pwnat/socket.c" "CMakeFiles/pwnat.dir/socket.c.o" "gcc" "CMakeFiles/pwnat.dir/socket.c.o.d"
+  "/home/hacker/pwnat/strlcpy.c" "CMakeFiles/pwnat.dir/strlcpy.c.o" "gcc" "CMakeFiles/pwnat.dir/strlcpy.c.o.d"
+  "/home/hacker/pwnat/udpclient.c" "CMakeFiles/pwnat.dir/udpclient.c.o" "gcc" "CMakeFiles/pwnat.dir/udpclient.c.o.d"
+  "/home/hacker/pwnat/udpserver.c" "CMakeFiles/pwnat.dir/udpserver.c.o" "gcc" "CMakeFiles/pwnat.dir/udpserver.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pwnat.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pwnat.dir/client.c.o"
+  "CMakeFiles/pwnat.dir/client.c.o.d"
+  "CMakeFiles/pwnat.dir/destination.c.o"
+  "CMakeFiles/pwnat.dir/destination.c.o.d"
+  "CMakeFiles/pwnat.dir/list.c.o"
+  "CMakeFiles/pwnat.dir/list.c.o.d"
+  "CMakeFiles/pwnat.dir/message.c.o"
+  "CMakeFiles/pwnat.dir/message.c.o.d"
+  "CMakeFiles/pwnat.dir/packet.c.o"
+  "CMakeFiles/pwnat.dir/packet.c.o.d"
+  "CMakeFiles/pwnat.dir/pwnat.c.o"
+  "CMakeFiles/pwnat.dir/pwnat.c.o.d"
+  "CMakeFiles/pwnat.dir/socket.c.o"
+  "CMakeFiles/pwnat.dir/socket.c.o.d"
+  "CMakeFiles/pwnat.dir/strlcpy.c.o"
+  "CMakeFiles/pwnat.dir/strlcpy.c.o.d"
+  "CMakeFiles/pwnat.dir/udpclient.c.o"
+  "CMakeFiles/pwnat.dir/udpclient.c.o.d"
+  "CMakeFiles/pwnat.dir/udpserver.c.o"
+  "CMakeFiles/pwnat.dir/udpserver.c.o.d"
+  "pwnat"
+  "pwnat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pwnat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
